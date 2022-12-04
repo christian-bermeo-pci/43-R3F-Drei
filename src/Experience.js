f7bff3b9a6de7edee091e1f8ac9681603166fe70
christian-bermeo-pci <christian.bermeo@premierconsultingllc.com>
2022-12-04 14:27:07 -0500

    R3F Drei

--- src/Experience.js
+++ src/Experience.js
@@ -19,6 +19,23 @@ export default function Experience() {
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
+      <mesh position-x={-2} ref={sphereRef}>
+        <Html
+          position={[1, 1, 0]}
+          wrapperClass={'sphereLabel'}
+          center
+          distanceFactor={6} // give it perspective on zoom in-out
+          occlude={[cubeRef, sphereRef]} // add refs of object that can hide it
+        >
+          That's a sphere 🚀
+        </Html>
+        <sphereGeometry />
+        <meshStandardMaterial color='orange' />
+      </mesh>
+
+      {/* Transform gizmo connect with ref */}
+      <TransformControls object={sphereRef} mode='translate' />
+
       <PivotControls
         anchor={[0, 0, 0]} //Position relative to the object
         depthTest={false}
@@ -26,27 +43,11 @@ export default function Experience() {
         axisColors={['#9381ff', '#ff4d6d', '#7ae582']} // line colors
         scale={1.5}
       >
-        <mesh position-x={-2} ref={sphereRef}>
-          <Html
-            position={[1, 1, 0]}
-            wrapperClass={'sphereLabel'}
-            center
-            distanceFactor={6} // give it perspective on zoom in-out
-            occlude={[cubeRef, sphereRef]} // add refs of object that can hide it
-          >
-            That's a sphere 🚀
-          </Html>
-          <sphereGeometry />
-          <meshStandardMaterial color='orange' />
+        <mesh ref={cubeRef} position-x={2} scale={1.5}>
+          <boxGeometry />
+          <meshStandardMaterial color='mediumpurple' />
         </mesh>
       </PivotControls>
-
-      <mesh ref={cubeRef} position-x={2} scale={1.5}>
-        <boxGeometry />
-        <meshStandardMaterial color='mediumpurple' />
-      </mesh>
-      {/* Transform gizmo connect with ref */}
-      <TransformControls object={cubeRef} mode='translate' />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
